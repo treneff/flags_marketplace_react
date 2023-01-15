54c9b1e18390ddb40e520aada821c79fdddc58e8
George Treneff <treneff@protonmail.com>
2023-01-15 14:01:19 +0000

    countriesToDisplay state fixed

--- src/components/FlagsList.js
+++ src/components/FlagsList.js
@@ -1,26 +1,31 @@
-import React from "react";
-import ListItem from "./ListItem";
-import "./FlagsList.css";
-import { useState } from "react";
-const FlagsList = ({ countries, addToBasket }) => {
-  const [itemsToDisplay, setItemsToDisplay] = useState(20);
-  const countryItems = countries.map((country, index) => {
-    if (index < itemsToDisplay) {
-      return (
-        <ListItem country={country} key={index} addToBasket={addToBasket} />
-      );
-    }
-    return null;
-  });
+import ListItem from './ListItem';
+import './FlagsList.css';
+import { useState, useEffect } from 'react';
 
-  // const showMoreItems = () => {
-  //     setItemsToDisplay(itemsToDisplay + 20);
-  // };
-  return (
-    <>
-      <ul className="shop-container">{countryItems}</ul>
-    </>
-  );
+const FlagsList = ({ countries, addToBasket }) => {
+    const [itemsToDisplay, setItemsToDisplay] = useState(20);
+    const [countriesToDisplay, setCountriesToDisplay] = useState([]);
+    console.log(countriesToDisplay);
+    console.log(countries);
+    useEffect(() => {
+        setCountriesToDisplay(countries);
+    }, [countries]);
+
+    const countryItems = countriesToDisplay.map((country, index) => {
+        if (index < itemsToDisplay) {
+            return <ListItem country={country} key={index} addToBasket={addToBasket} />;
+        }
+        return null;
+    });
+
+    // const showMoreItems = () => {
+    //     setItemsToDisplay(itemsToDisplay + 20);
+    // };
+    return (
+        <>
+            <ul className='shop-container'>{countryItems}</ul>
+        </>
+    );
 };
 
 export default FlagsList;
