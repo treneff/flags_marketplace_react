f1261cda52b223b2d073d2c005bb777770e48bbe
George Treneff <treneff@protonmail.com>
2023-01-15 16:30:50 +0000

    :sparkles: full search by name+ show more

--- src/components/FlagsList.js
+++ src/components/FlagsList.js
@@ -1,29 +1,48 @@
 import ListItem from './ListItem';
+
 import './FlagsList.css';
 import { useState, useEffect } from 'react';
 
 const FlagsList = ({ countries, addToBasket }) => {
-    const [itemsToDisplay, setItemsToDisplay] = useState(20);
-    const [countriesToDisplay, setCountriesToDisplay] = useState([]);
-    console.log(countriesToDisplay);
+    const [maxItemsToDisplay, setMaxItemsToDisplay] = useState(20);
+    const [filteredCountries, setFilteredCountries] = useState([]);
+    const [searchCountry, setSearchCountry] = useState('');
+    console.log(filteredCountries);
     console.log(countries);
-    useEffect(() => {
-        setCountriesToDisplay(countries);
-    }, [countries]);
+
+    const countriesToDisplay = searchCountry ? filteredCountries : countries;
 
     const countryItems = countriesToDisplay.map((country, index) => {
-        if (index < itemsToDisplay) {
+        if (index < maxItemsToDisplay) {
             return <ListItem country={country} key={index} addToBasket={addToBasket} />;
         }
         return null;
     });
 
-    // const showMoreItems = () => {
-    //     setItemsToDisplay(itemsToDisplay + 20);
-    // };
+    const handleChange = (event) => {
+        setSearchCountry(event.target.value);
+        let filteredSearch = countries.filter((country) => {
+            return country.name.common.toLowerCase().includes(event.target.value.toLowerCase());
+        });
+        setFilteredCountries(filteredSearch);
+        setMaxItemsToDisplay(20);
+    };
+
+    const showMoreItems = () => {
+        setMaxItemsToDisplay(maxItemsToDisplay + 20);
+    };
     return (
         <>
+            <form>
+                <input
+                    type='text'
+                    placeholder='Search...'
+                    value={searchCountry}
+                    onChange={handleChange}
+                />
+            </form>
             <ul className='shop-container'>{countryItems}</ul>
+            <button onClick={showMoreItems}>Show More</button>
         </>
     );
 };
